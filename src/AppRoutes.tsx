9de69c6c4dfbfa314e642e4c471be138efab35f0
Mashookh <maxpain45545@gmail.com>
2025-04-29 22:25:49 +0530

    first commit

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,31 @@
+
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Layout from './layouts/Layout'
+import Homepage from './pages/Homepage'
+import AuthCallbackPage from './pages/AuthCallbackPage'
+import UserProfilePage from './pages/UserProfilePage'
+import ProtectedRoute from './auth/ProtectedRoute'
+import ManageRestaurantPage from './pages/ManageRestaurantPage'
+import SearchPage from './pages/SearchPage'
+import DetailPage from './pages/DetailPage'
+import OrderStatusPage from './pages/OrderStatusPage'
+
+function AppRoutes() {
+  return (
+    <Routes>
+        <Route path="/" element={<Layout showHero><Homepage/></Layout>}/>
+        <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
+        <Route path="/search/:city" element={<Layout showHero={false}><SearchPage/></Layout>}/>
+        <Route path="/detail/:restaurantId" element={<Layout showHero={false}><DetailPage/></Layout>}/>
+        <Route element={<ProtectedRoute/>}>
+          <Route path="/user-profile" element={<Layout showHero= {false}><UserProfilePage/></Layout>}/>
+          <Route path="/order-status" element={<Layout showHero= {false}><OrderStatusPage/></Layout>}/>
+          <Route path="/manage-restaurant" element={<Layout><ManageRestaurantPage/></Layout>}/>
+        </Route>
+        
+        <Route path="*" element={<Navigate to="/"/>}/>
+    </Routes>
+  )
+}
+
+export default AppRoutes
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,25 @@
+
+import SearchBar, {SearchForm} from '@/components/SearchBar';
+import { useNavigate } from 'react-router-dom';
+
+const Homepage = () => {
+    const navigate = useNavigate()
+    const handleSearchSubmit = ( searchFormValues: SearchForm )=>{
+        navigate({
+            pathname : `/search/${searchFormValues.searchQuery}`
+        })
+    }
+  return(
+    <div className="flex flex-col gap-12">
+        <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+            <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+                Tuck into the takeway today
+            </h1>
+            <span className='text-xl'>Food is just a click away!</span>
+            <SearchBar placeHolder='Search by city or town ' onSubmit={handleSearchSubmit} searchQuery={''}/>
+        </div>
+    </div>
+  )
+}
+
+export default Homepage;
